2155eb484b8b168a834dabbcd2edeb5f8078a927
Krzysztof Langner <klangner@gmail.com>
2013-08-14 11:14:43 +0200

    ustawinie domyślnego url po logowaniu

--- src/bluenotepad/settings.py
+++ src/bluenotepad/settings.py
@@ -107,6 +107,8 @@ TEMPLATE_DIRS = (
     os.path.join(os.path.dirname(__file__), 'templates'),
 )
 
+LOGIN_REDIRECT_URL = '/notepad'
+
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
